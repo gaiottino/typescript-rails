e3b5d62337ca1705221bf9f14a05fbbd56587ac3
Daniel Gaiottino <daniel.gaiottino@gmail.com>
2015-12-23 16:24:07 +0100

    Return js if exists and newer than ts

--- lib/typescript/rails/compiler.rb
+++ lib/typescript/rails/compiler.rb
@@ -4,15 +4,20 @@ require 'typescript/rails'
 module Typescript::Rails::Compiler
   class << self
     def compile(ts_path, source, context=nil, *options)
-      stdout, stderr, exit_status = Open3.capture3('/usr/local/bin/tsc')
+      path = File.join(Rails.root.to_s, 'app', 'assets', 'typescripts')
+      relative_path = ts_path.gsub(path, '')
+      js_path = File.join(Rails.root, 'tmp', 'typescript_rails', relative_path).gsub('.ts', '.js')
 
-      if exit_status == 0
-        path = File.join(Rails.root.to_s, 'app', 'assets', 'typescripts')
-        relative_path = ts_path.gsub(path, '')
-        javascript_file = File.open(File.join(Rails.root, 'tmp', 'typescript_rails', relative_path).gsub('.ts', '.js'))
-        javascript_file.read
+      if File.exist?(js_path) && File.mtime(js_path) > File.mtime(ts_path)
+        File.open(js_path, 'r').read
       else
-        raise stderr + stdout
+        Rails.logger.debug 'Typescript::Rails::Compiler will compile all .ts files.'
+        stdout, stderr, exit_status = Open3.capture3('/usr/local/bin/tsc -w')
+        if exit_status == 0
+          File.open(js_path, 'r').read
+        else
+          raise stderr + stdout
+        end
       end
     end
   end
